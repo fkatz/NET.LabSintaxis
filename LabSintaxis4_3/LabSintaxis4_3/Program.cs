97d1c783b7cd2455dfc42f47c8dc2ee4096db8c5
theherno <hernanlistorti@gmail.com>
2019-03-31 16:31:12 -0300

    .

--- LabSintaxis4_3/LabSintaxis4_3/Program.cs
+++ LabSintaxis4_3/LabSintaxis4_3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LabSintaxis4_3
 {
@@ -10,6 +6,16 @@ namespace LabSintaxis4_3
     {
         static void Main(string[] args)
         {
+            int limit, a=0, b=1, aux, sum=0;
+            Console.WriteLine("SUMA DE SERIE DE FIBONACCI\nIngrese cantidad de números de la serie a sumar: ");
+            limit = Int32.Parse(Console.ReadLine());
+            for(int i=0; i<limit; i++) {
+                aux = a;
+                a = b; 
+                b = auxiliar + a; 
+                sum+=a;
+            }
+            Console.WriteLine("La suma es: "+sum);
         }
     }
 }
