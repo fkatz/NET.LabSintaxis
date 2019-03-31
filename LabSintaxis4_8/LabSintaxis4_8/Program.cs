a299b86d70768d84c5977c4df9f6aa5feedeba0b
theherno <hernanlistorti@gmail.com>
2019-03-31 18:00:54 -0300

    .

---
+++ LabSintaxis4_8/LabSintaxis4_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LabSintaxis4_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            string clave;
+            do
+            {
+                intentos++;
+                Console.WriteLine("Introduzca Clave");
+                clave = Console.ReadLine();
+            } while (intentos < 4 && clave != "correcta");
+            if (clave == "correcta")
+            {
+                Console.WriteLine("Clave Correcta");
+                Console.ReadKey();
+            }
+        }
+    }
+}
